85a7609b66c3dd8bf09c52c30b609004b02f3e7c
anfulu36484 <anfulu36484@gmail.com>
2016-04-27 23:10:39 +0400

    fixed shinigami class

---
+++ iichanTouhou/Objects/NPC/NPC_Standart.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using IIchanDanmakuProject.Attack.PolarAttack;
+using IIchanDanmakuProject.Attack.StandartAttack;
+using SFML.System;
+
+namespace IIchanDanmakuProject.Objects.NPC
+{
+    class NPC_Standart :GameObject
+    {
+        public NPC_Standart(Danmaku danmaku, Vector2f startPosition, Vector2f size, float hitboxRadius, int lifeTime)
+            : base(danmaku, startPosition, size, hitboxRadius, lifeTime)
+        {
+        }
+
+
+        public override void Initialize()
+        {
+            Texture = danmaku.Textures["npc"];
+
+            Speed =new Vector2f(0,2f);
+        }
+
+
+        public override void Update()
+        {
+            base.Update();
+
+            if (LivedTimeInSeconds == 2)
+            {
+                base.RectangleShape.Texture = danmaku.Textures["bullet2"];
+
+                Speed = new Vector2f(0, 0);
+                if (_attack1 == null)
+                    Attack();
+            }
+
+            _attack1?.Update();
+
+            
+        }
+
+        private Attack1 _attack1;
+
+        public void Attack()
+        {
+            _attack1 = new Attack1(danmaku, this, CenterCoordinates, int.MaxValue/danmaku.FrameRateLimit, 50);
+            _attack1.Initialize();
+            _attack1.Died += FlowerDied;
+        }
+
+        private void FlowerDied(object sender, EventArgs e)
+        {
+            _attack1 = null;
+        }
+
+        public override void Render()
+        {
+            base.Render();
+            _attack1?.Render();
+        }
+
+
+        
+    }
+}
--- iichanTouhou/Objects/ObjectsDeath/Shinigami.cs
+++ iichanTouhou/Objects/ObjectsDeath/Shinigami.cs
@@ -16,13 +16,13 @@ namespace IIchanDanmakuProject.Objects.ObjectsDeath
         {
         }
 
-        private float speedFactor = 5;
+        private float _speedFactor = 5;
 
         public void Add(BulletBase bullet)
         {
             if (bullet.IsObjectInGameArea())
             {
-                bullet.Speed = (bullet.CenterCoordinates - bullet.OwnerObject.CenterCoordinates).Normalize()*speedFactor;
+                bullet.Speed = (bullet.CenterCoordinates - bullet.OwnerObject.CenterCoordinates).Normalize()*_speedFactor;
                 _objects.Add(bullet);
             }
         }
@@ -37,44 +37,34 @@ namespace IIchanDanmakuProject.Objects.ObjectsDeath
         }
 
 
-        List<BulletBase> _objectsForRemoval=new List<BulletBase>();
 
         private void OnCollision(object sender, EventArgs eventArgs)
         {
-            _objectsForRemoval.Add((BulletBase)sender);
+            _objects.Remove((BulletBase) sender);
         }
 
 
         public override void Initialize() {}
 
 
-        private void CompletelyRemoveObjects()
-        {
-            BulletBase[] objectsForRemovalTemp = _objectsForRemoval.ToArray();
-
-            for (int i = 0; i < objectsForRemovalTemp.Length; i++)
-            {
-                _objects.Remove(objectsForRemovalTemp[i]);
-                _objectsForRemoval.Remove(objectsForRemovalTemp[i]);
-            }
-        }
+        private object _lockObj = new object();
 
         public override void Update()
         {
-            for (int i = 0; i < _objects.Count; i++)
+            lock (_lockObj)
             {
-                if (!_objects[i].IsObjectInGameArea())
+                for (int i = _objects.Count - 1; i > -1; i--)
                 {
-                    _objectsForRemoval.Add(_objects[i]);
+                    if (!_objects[i].IsObjectInGameArea())
+                    {
+                        _objects.Remove(_objects[i]);
+                    }
+                    else
+                    {
+                        _objects[i].Update();
+                    }
                 }
-                else
-                {
-                    _objects[i].Update();
-                }
-
             }
-
-            CompletelyRemoveObjects();
         }
 
         
@@ -83,7 +73,7 @@ namespace IIchanDanmakuProject.Objects.ObjectsDeath
         {
             foreach (var obj in _objects)
             {
-                obj?.Render();
+                obj.Render();
             }
         }
 
