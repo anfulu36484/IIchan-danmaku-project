e3efb11e43c45ccf974a7cee4514953bf679ada8
anfulu36484 <anfulu36484@gmail.com>
2016-05-07 23:26:35 +0400

    add focused behevior for PowerSphereAttack

--- iichanTouhou/Attack/AttackOfMainObject/PowerSphere/PowerSphereHolder.cs
+++ iichanTouhou/Attack/AttackOfMainObject/PowerSphere/PowerSphereHolder.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using IIchanDanmakuProject.Helpers;
 using IIchanDanmakuProject.Objects;
 using IIchanDanmakuProject.Objects.Bullets;
--- iichanTouhou/Objects/Bullets/MainObjectBullets/BulletForPowerSphere.cs
+++ iichanTouhou/Objects/Bullets/MainObjectBullets/BulletForPowerSphere.cs
@@ -5,11 +5,15 @@ using IIchanDanmakuProject.Objects.Bullets.Behavior.DirectionOfMovement;
 using IIchanDanmakuProject.Objects.Bullets.Behavior.Rotate;
 using IIchanDanmakuProject.Objects.Bullets.Behavior.WayOfDying;
 using SFML.System;
+using SFML.Window;
 
 namespace IIchanDanmakuProject.Objects.Bullets.MainObjectBullets
 {
     class BulletForPowerSphere :BulletBase
     {
+        private DeterminantOfDirectionOfMovementBase _focusedMovement;
+        private DeterminantOfDirectionOfMovementBase _anfocusedMovement;
+
         public BulletForPowerSphere(Danmaku danmaku, Vector2f startPosition, 
             List<GameObject> targetObjects, GameObject ownerObject, EventHandler<EventArgs> onCollision) 
             : base(danmaku, startPosition, new Vector2f(50,50), 25, targetObjects, ownerObject, onCollision, 
@@ -19,13 +23,24 @@ namespace IIchanDanmakuProject.Objects.Bullets.MainObjectBullets
                   danmaku.Textures["bulletmainobject3"],
                   new NoneWayOfDying(danmaku),new StatChanger(-1))
         {
-            
+  
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            DeterminantOfDirectionOfMovement = Keyboard.IsKeyPressed(Keyboard.Key.LShift) ? _focusedMovement : _anfocusedMovement;
         }
 
         public override void Initialize()
         {
             DeterminantOfDirectionOfMovement.SpeedFactor = 10;
             DeterminantOfDirectionOfMovement.Initialize(this);
+            _anfocusedMovement = DeterminantOfDirectionOfMovement;
+            _focusedMovement = new MovementInPredeterminedDirection(new Vector2f(0, -1));
+            _focusedMovement.Initialize(this);
+            _focusedMovement.SpeedFactor = _anfocusedMovement.SpeedFactor;
         }
+
     }
 }
